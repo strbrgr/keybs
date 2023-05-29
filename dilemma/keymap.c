f4f0df072aad1c685c1fab4717ec9c701ad8b223
Jochen Stierberger <jochen@stierberger.com>
2023-05-29 06:50:14 -0500

    Updates to Pointer Layer

--- dilemma/keymap.c
+++ dilemma/keymap.c
@@ -14,20 +14,23 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include QMK_KEYBOARD_H
-#include "layer_names.h"
-#include "keycodes.h"
 
 #ifdef DILEMMA_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 #    include "timer.h"
 #endif // DILEMMA_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
-#define NAV MO(_NAV)
-#define SYM MO(_SYM)
+enum dilemma_keymap_layers {
+    LAYER_BASE = 0,
+    LAYER_FUNCTION,
+    LAYER_NAVIGATION,
+    LAYER_POINTER,
+    LAYER_NUMERAL,
+    LAYER_SYMBOLS,
+};
 
 // Automatically enable sniping-mode on the pointer layer.
-#define DILEMMA_AUTO_SNIPING_ON_LAYER _POINTER
+#define DILEMMA_AUTO_SNIPING_ON_LAYER LAYER_POINTER
 
 #ifdef DILEMMA_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 static uint16_t auto_pointer_layer_timer = 0;
@@ -41,6 +44,12 @@ static uint16_t auto_pointer_layer_timer = 0;
 #    endif // DILEMMA_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
 #endif     // DILEMMA_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 
+#define SPC_NAV LT(LAYER_NAVIGATION, KC_SPC)
+#define TAB_FUN LT(LAYER_FUNCTION, KC_TAB)
+#define ESC_SYM LT(LAYER_SYMBOLS, KC_ESC)
+#define D_NUM LT(LAYER_NUMERAL, KC_D)
+#define _L_PTR(KC) LT(LAYER_POINTER, KC)
+
 #ifndef POINTING_DEVICE_ENABLE
 #    define DRGSCRL KC_NO
 #    define DPI_MOD KC_NO
@@ -49,72 +58,175 @@ static uint16_t auto_pointer_layer_timer = 0;
 #endif // !POINTING_DEVICE_ENABLE
 
 // clang-format off
+/** \brief QWERTY layout (3 rows, 10 columns). */
+#define LAYOUT_LAYER_BASE                                                                     \
+       KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, \
+       KC_A,    KC_R,    KC_S,    KC_T,    ALL_T(KC_G),    ALL_T(KC_M),    KC_N,    KC_E,    KC_I, KC_O, \
+       KC_Z,    KC_X,    KC_C,    D_NUM,    KC_V,    KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH, \
+                               ESC_SYM, SPC_NAV, KC_ENT, KC_BSPC
+
+/** Convenience row shorthands. */
+#define _______________DEAD_HALF_ROW_______________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+#define ______________HOME_ROW_GACS_L______________ KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX
+#define ______________HOME_ROW_GACS_R______________ XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI
+
+/*
+ * Layers used on the Dilemma.
+ *
+ * These layers started off heavily inspired by the Miryoku layout, but trimmed
+ * down and tailored for a stock experience that is meant to be fundation for
+ * further personalization.
+ *
+ * See https://github.com/manna-harbour/miryoku for the original layout.
+ */
+
+/**
+ * \brief Function layer.
+ *
+ * Secondary right-hand layer has function keys mirroring the numerals on the
+ * primary layer with extras on the pinkie column, plus system keys on the inner
+ * column. App is on the tertiary thumb key and other thumb keys are duplicated
+ * from the base layer to enable auto-repeat.
+ */
+#define LAYOUT_LAYER_FUNCTION                                                                 \
+    _______________DEAD_HALF_ROW_______________, KC_PSCR,   KC_F7,   KC_F8,   KC_F9,  KC_F12, \
+    ______________HOME_ROW_GACS_L______________, KC_SCRL,   KC_F4,   KC_F5,   KC_F6,  KC_F11, \
+    _______________DEAD_HALF_ROW_______________, KC_PAUS,   KC_F1,   KC_F2,   KC_F3,  KC_F10, \
+                               _______, XXXXXXX, XXXXXXX, XXXXXXX
+
+/** \brief Mouse emulation and pointer functions. */
+#define LAYOUT_LAYER_POINTER                                                                  \
+    QK_BOOT, XXXXXXX, XXXXXXX, DPI_MOD, S_D_MOD, S_D_MOD, DPI_MOD, XXXXXXX, XXXXXXX, QK_BOOT, \
+    ______________HOME_ROW_GACS_L______________, ______________HOME_ROW_GACS_R______________, \
+    _______, DRGSCRL, SNIPING, KC_BTN3, XXXXXXX, XXXXXXX, KC_BTN3, SNIPING, DRGSCRL, _______, \
+                               KC_BTN2, KC_BTN1, KC_BTN1, KC_BTN2
+
+/**
+ * \brief Navigation layer.
+ *
+ * Primary right-hand layer (left home thumb) is navigation and editing. Cursor
+ * keys are on the home position, line and page movement below, clipboard above,
+ * caps lock and insert on the inner column. Thumb keys are duplicated from the
+ * base layer to avoid having to layer change mid edit and to enable auto-repeat.
+ */
+#define LAYOUT_LAYER_NAVIGATION                                                               \
+    _______________DEAD_HALF_ROW_______________, XXXXXXX, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, \
+    ______________HOME_ROW_GACS_L______________, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, \
+    _______________DEAD_HALF_ROW_______________,  XXXXXXX,KC_LBRC, KC_RBRC, KC_GT, KC_LT, \
+                               XXXXXXX, _______,  XXXXXXX, XXXXXXX
+
+/**
+ * \brief Numeral layout.
+ *
+ * Primary left-hand layer (right home thumb) is numerals and symbols. Numerals
+ * are in the standard numpad locations with symbols in the remaining positions.
+ * `KC_DOT` is duplicated from the base layer.
+ */
+#define LAYOUT_LAYER_NUMERAL                                                                  \
+    _______________DEAD_HALF_ROW_______________,  XXXXXXX,  KC_7, KC_8,  KC_9, XXXXXXX, \
+    _______________DEAD_HALF_ROW_______________,  XXXXXXX,  KC_4, KC_5,  KC_6, XXXXXXX, \
+     _______________DEAD_HALF_ROW_______________,    KC_0,  KC_1, KC_2,  KC_3, XXXXXXX, \
+                               KC_MINS,     KC_0, XXXXXXX, _______
+
+/**
+ * \brief Symbols layer.
+ *
+ * Secondary left-hand layer has shifted symbols in the same locations to reduce
+ * chording when using mods with shifted symbols. `KC_LPRN` is duplicated next to
+ * `KC_RPRN`.
+ */
+#define LAYOUT_LAYER_SYMBOLS                                                                  \
+    _______________DEAD_HALF_ROW_______________, KC_CIRC, KC_PERC, KC_ASTR, KC_AMPR, KC_BSLS, \
+    _______________DEAD_HALF_ROW_______________, KC_GRV, KC_QUOT, KC_DQUO, KC_EXLM, KC_PLUS, \
+    _______________DEAD_HALF_ROW_______________, KC_TILD, KC_HASH, KC_AT, KC_DLR, KC_PIPE, \
+                                KC_GRV, KC_UNDS, KC_MINS, KC_EQL
+
+/**
+ * \brief Add Home Row mod to a layout.
+ *
+ * Expects a 10-key per row layout.  Adds support for GACS (Gui, Alt, Ctl, Shift)
+ * home row.  The layout passed in parameter must contain at least 20 keycodes.
+ *
+ * This is meant to be used with `LAYER_ALPHAS_QWERTY` defined above, eg.:
+ *
+ *     HOME_ROW_MOD_GACS(LAYER_ALPHAS_QWERTY)
+ */
+#define _HOME_ROW_MOD_GACS(                                            \
+    L00, L01, L02, L03, L04, R05, R06, R07, R08, R09,                  \
+    L10, L11, L12, L13, L14, R15, R16, R17, R18, R19,                  \
+    ...)                                                               \
+             L00,         L01,         L02,         L03,         L04,  \
+             R05,         R06,         R07,         R08,         R09,  \
+      LGUI_T(L10), LALT_T(L11), LCTL_T(L12), LSFT_T(L13),        L14,  \
+             R15,  RSFT_T(R16), RCTL_T(R17), LALT_T(R18), RGUI_T(R19), \
+      __VA_ARGS__
+#define HOME_ROW_MOD_GACS(...) _HOME_ROW_MOD_GACS(__VA_ARGS__)
+
+/**
+ * \brief Add pointer layer keys to a layout.
+ *
+ * Expects a 10-key per row layout.  The layout passed in parameter must contain
+ * at least 30 keycodes.
+ *
+ * This is meant to be used with `LAYER_ALPHAS_QWERTY` defined above, eg.:
+ *
+ *     POINTER_MOD(LAYER_ALPHAS_QWERTY)
+ */
+#define _POINTER_MOD(                                                  \
+    L00, L01, L02, L03, L04, R05, R06, R07, R08, R09,                  \
+    L10, L11, L12, L13, L14, R15, R16, R17, R18, R19,                  \
+    L20, L21, L22, L23, L24, R25, R26, R27, R28, R29,                  \
+    ...)                                                               \
+             L00,         L01,         L02,         L03,         L04,  \
+             R05,         R06,         R07,         R08,         R09,  \
+             L10,         L11,         L12,         L13,         L14,  \
+             R15,         R16,         R17,         R18,         R19,  \
+      _L_PTR(L20),        L21,         L22,         L23,         L24,  \
+             R25,         R26,         R27,         R28,  _L_PTR(R29), \
+      __VA_ARGS__
+#define POINTER_MOD(...) _POINTER_MOD(__VA_ARGS__)
+
+#define LAYOUT_wrapper(...) LAYOUT_split_3x5_2(__VA_ARGS__)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT_split_3x5_2(
-  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-          KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,       KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-      LGUI_KA,  LALT_KR, LCTL_KS, LSFT_KT,  ALL_KG,     ALL_KM, RSFT_KN, RCTL_KE, RALT_KI, RGUI_KO,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-          KC_Z,    KC_X,    KC_C,   LNU_D,    KC_V,       KC_K,    KC_H, KC_COMM,  KC_DOT, KC_SLSH,
-  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                                   LSY_ESC, LNA_SPC,    KC_ENT,  KC_BSPC
-  //                            ╰──────────────────╯ ╰──────────────────╯
+  [LAYER_BASE] = LAYOUT_wrapper(
+    POINTER_MOD(HOME_ROW_MOD_GACS(LAYOUT_LAYER_BASE))
   ),
-
-  [_SYM] = LAYOUT_split_3x5_2(
-  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_CIRC, KC_CIRC, KC_ASTR, KC_AMPR, KC_BSLS,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,    KC_GRV, KC_QUOT, KC_DQUO, KC_EXLM, KC_PLUS,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX,    KC_TILD, KC_HASH, KC_AT, KC_DLR, KC_PIPE,
-  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                                  _______, XXXXXXX,    XXXXXXX, _______
-  //                            ╰──────────────────╯ ╰──────────────────╯
-  ),
-
-  [_NUM] = LAYOUT_split_3x5_2(
-  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-      XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,   XXXXXXX,   KC_7,   KC_8,   KC_9, XXXXXXX,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-      XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,     XXXXXXX, KC_4,   KC_5,   KC_6,  XXXXXXX,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-      XXXXXXX,  XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,      KC_0,   KC_1,   KC_2,   KC_3,  XXXXXXX,
-  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                                  _______, KC_LSFT,     KC_SPC, _______
-  //                            ╰──────────────────╯ ╰──────────────────╯
-  ),
-
-  [_NAV] = LAYOUT_split_3x5_2(
-  // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,   XXXXXXX,
-  // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-       XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX, KC_LBRC, KC_RBRC, KC_GT,    KC_LT,
-  // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                                  _______, XXXXXXX,     XXXXXXX, _______
-  //                            ╰──────────────────╯ ╰──────────────────╯
-  ),
-
-  [_POINTER] = LAYOUT_split_3x5_2(
-    // ╭─────────────────────────────────────────────╮ ╭─────────────────────────────────────────────╮
-        QK_BOOT, XXXXXXX, XXXXXXX, DPI_MOD, S_D_MOD,     S_D_MOD, DPI_MOD, XXXXXXX, XXXXXXX,QK_BOOT,
-    // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-        KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,   KC_F11,     KC_F12, KC_RGUI, KC_RALT, KC_RCTL, KC_RSFT,
-    // ├─────────────────────────────────────────────┤ ├─────────────────────────────────────────────┤
-         _______, DRGSCRL, SNIPING, KC_BTN3, XXXXXXX,    XXXXXXX, KC_BTN3, SNIPING, DRGSCRL,  _______,
-    // ╰─────────────────────────────────────────────┤ ├─────────────────────────────────────────────╯
-                                    KC_BTN2, KC_BTN1,     KC_BTN1, KC_BTN2
-    //                            ╰──────────────────╯ ╰──────────────────╯
-    ),
+  [LAYER_FUNCTION] = LAYOUT_wrapper(LAYOUT_LAYER_FUNCTION),
+  [LAYER_NAVIGATION] = LAYOUT_wrapper(LAYOUT_LAYER_NAVIGATION),
+  [LAYER_NUMERAL] = LAYOUT_wrapper(LAYOUT_LAYER_NUMERAL),
+  [LAYER_POINTER] = LAYOUT_wrapper(LAYOUT_LAYER_POINTER),
+  [LAYER_SYMBOLS] = LAYOUT_wrapper(LAYOUT_LAYER_SYMBOLS),
 };
 // clang-format on
 
-layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, _NAV, _SYM, _NUM);
+#ifdef POINTING_DEVICE_ENABLE
+#    ifdef DILEMMA_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    if (abs(mouse_report.x) > DILEMMA_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > DILEMMA_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
+        if (auto_pointer_layer_timer == 0) {
+            layer_on(LAYER_POINTER);
+        }
+        auto_pointer_layer_timer = timer_read();
+    }
+    return mouse_report;
 }
+
+void matrix_scan_user(void) {
+    if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= DILEMMA_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
+        auto_pointer_layer_timer = 0;
+        layer_off(LAYER_POINTER);
+    }
+}
+#    endif // DILEMMA_AUTO_POINTER_LAYER_TRIGGER_ENABLE
+
+#    ifdef DILEMMA_AUTO_SNIPING_ON_LAYER
+layer_state_t layer_state_set_user(layer_state_t state) {
+    dilemma_set_pointer_sniping_enabled(layer_state_cmp(state, DILEMMA_AUTO_SNIPING_ON_LAYER));
+    return state;
+}
+#    endif // DILEMMA_AUTO_SNIPING_ON_LAYER
+#endif     // POINTING_DEVICE_ENABLE
 
 enum combo_events {
   SCREENSHOT,
--- dilemma/layer_names.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-enum my_layers {
-    _BASE,
-    _SYM,
-    _NUM,
-    _NAV,
-    _POINTER,
-};
